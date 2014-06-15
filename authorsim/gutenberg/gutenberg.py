13daf385556dbd0db2ac31fed47e47ed24c62896
c-w <clemens.wolff+git@gmail.com>
2014-06-15 19:07:39 +0100

    Hide implementation of the cleaning up method
    Only expose a wrapper around strip_headers to the outside world.  This makes the
    public interface of the gutenberg module smaller and makes it easier to use some
    of the functionality in the module as stand-alone scripts.

--- authorsim/gutenberg/gutenberg.py
+++ authorsim/gutenberg/gutenberg.py
@@ -1,64 +1,13 @@
-# -*- coding: utf-8 -*-
-
-from terminators import HEADERS, FOOTERS
-
-
-def strip_headers(lines):
-    """Remove lines that are part of the Project Gutenberg header or footer.
-
-    This is a port of the C++ version by Johannes Krugel
-    Link: http://www14.in.tum.de/spp1307/src/strip_headers.cpp
-
-    """
-    out = []
-    i = 0
-    reset = True
-    footer_found = False
-
-    for line in lines:
-        if len(line) <= 12:
-            continue  # just a shortcut for short lines
-
-        reset = False
-
-        if i <= 600:
-            # Check if the header ends here
-            if any(line.startswith(header) for header in HEADERS):
-                reset = True
-
-            # If it's the end of the header, delete the output produced so far.
-            # May be done several times, if multiple lines occur indicating the
-            # end of the header
-            if reset:
-                out = []
-                continue
-
-        if i >= 100:
-            # Check if the footer begins here
-            if any(line.startswith(footer) for footer in FOOTERS):
-                footer_found = True
-
-            # If it's the beginning of the footer, stop output
-            if footer_found:
-                break
-
-        out.append(line)
-        i += 1
-
-    return out
+from beautifier import strip_headers
+import os
+import urllib
 
 
-if __name__ == '__main__':
-    import argparse
-    import sys
-
-    doc = 'Removes Project Gutenberg headers and footers.'
-    parser = argparse.ArgumentParser(description=doc)
-    parser.add_argument('infile', nargs='?', type=argparse.FileType('r'),
-                        default=sys.stdin, help='Gutenberg text to clean-up')
-    parser.add_argument('outfile', nargs='?', type=argparse.FileType('w'),
-                        default=sys.stdout, help='Cleaned text goes here')
-    args = parser.parse_args()
-
-    for line in strip_headers(args.infile):
-        args.outfile.write(line)
+def cleanup(inp):
+    try:
+        text = urllib.urlopen(inp).read()
+    except IOError:
+        text = inp
+    lines = text.split(os.linesep)
+    clean = strip_headers(lines)
+    return os.linesep.join(clean)
