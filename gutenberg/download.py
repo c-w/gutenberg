3720e21f6e9fb536ff4690453d524b1d0c4edf33
c-w <clemens.wolff+git@gmail.com>
2014-06-18 00:29:28 +0100

    Inline constants

--- gutenberg/download.py
+++ gutenberg/download.py
@@ -1,4 +1,3 @@
-from useragents import USER_AGENTS
 import bs4
 import errno
 import logging
@@ -9,6 +8,16 @@ import requests
 import time
 import urllib
 import urlparse
+
+
+USER_AGENTS = [
+    'Mozilla/5.0 (Windows; U; Windows NT 5.1; it; rv:1.8.1.11) Gecko/20071127 Firefox/2.0.0.11',  # noqa
+    'Opera/9.25 (Windows NT 5.1; U; en)',  # noqa
+    'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 1.1.4322; .NET CLR 2.0.50727)',  # noqa
+    'Mozilla/5.0 (compatible; Konqueror/3.5; Linux) KHTML/3.5.5 (like Gecko) (Kubuntu)',  # noqa
+    'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.8.0.12) Gecko/20070731 Ubuntu/dapper-security Firefox/1.5.0.12',  # noqa
+    'Lynx/2.8.5rel.1 libwww-FM/2.14 SSL-MM/1.4.1 GNUTLS/1.2.9',  # noqa
+]
 
 
 def gutenberg_links(filetypes, langs, offset):
