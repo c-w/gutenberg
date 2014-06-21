50b41e3da4b5ab45c5724a637cd4f45fdb164380
c-w <clemens.wolff+git@gmail.com>
2014-06-21 17:34:55 +0100

    File handling utilities

--- gutenberg/common.py
+++ gutenberg/common.py
@@ -1,8 +1,11 @@
 import errno
 import functools
+import gzip
+import itertools
 import os
 import re
 import urlparse
+import zipfile
 
 
 def memoize(fun):
@@ -31,6 +34,55 @@ def memoize(fun):
         return value
 
     return memoizer
+
+
+def readfile(path):
+    """Opens a file. This is a wrapper around various file-opening methods that
+    automatically determines which opener to use depending on the file's magic
+    number.
+
+    Args:
+        path (str): the path of the file to open
+
+    Returns:
+        iter: an iterator over the lines in the file
+
+    """
+    with open(path, 'rb') as binary_file:
+        magic = str(binary_file.read(4).encode('hex')).upper()
+
+    if magic.startswith('504B0304'):
+        zipf = zipfile.ZipFile(path)
+        try:
+            return itertools.chain(*[zipf.open(f) for f in zipf.namelist()])
+        finally:
+            zipf.close()
+
+    if magic.startswith('1F8B08'):
+        with gzip.open(path) as gzipf:
+            return iter(gzipf)
+
+    raise ValueError(
+        'Unsupported file with extension {ext} and magic number {magic}'
+        .format(ext=os.path.splitext(path)[1], magic=magic))
+
+
+def listfiles(root):
+    """Lists all the files in a directory and all of its subdirectories.
+
+    Args:
+        root (str): the top-level directory from which to list files
+
+    Returns:
+        iter: an iterator over the paths to all files under the top-level
+              directory
+
+    """
+    for dirpath, subdirs, filenames in os.walk(root):
+        for filename in filenames:
+            yield os.path.join(dirpath, filename)
+        for subdir in subdirs:
+            listfiles(subdir)
 
 
 def makedirs(path):
