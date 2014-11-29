6e04d3ab8a6d967c14afaf45869152c93d94d1ec
Clemens Wolff <clemens.wolff+git@gmail.com>
2014-11-29 12:02:24 +0000

    Refactor remote URI formatting functions
    - Whitespace
    - Extract functions into single-responsibility units

--- gutenberg/textsource.py
+++ gutenberg/textsource.py
@@ -12,6 +12,49 @@ import rdflib
 import tarfile
 
 
+def _is_legacy_uid(uid):
+    return 0 < uid < 10
+
+
+def _format_uri(uid):
+    if _is_legacy_uid(uid):
+        raise ValueError('should use legacy URI format for UIDs in (0..10)')
+
+    uid = str(uid)
+    return '{root}/{path}/{uid}/{uid}.txt'.format(
+        root=r'http://www.gutenberg.lib.md.us',
+        path='/'.join(uid[:len(uid) - 1]),
+        uid=uid)
+
+
+def _format_legacy_uri(uid):
+    if not _is_legacy_uid(uid):
+        raise ValueError('should use non-legacy URI format for UIDs >= 10')
+
+    legacy_files = (
+        'when11',
+        'bill11',
+        'jfk11',
+        'getty11',
+        'const11',
+        'liber11',
+        'mayfl11',
+        'linc211',
+        'linc111',
+    )
+    return '{root}/{path}/{name}.txt'.format(
+        root=r'http://www.gutenberg.lib.md.us',
+        path='etext90',
+        name=legacy_files[uid - 1])
+
+
+def remote_uri_formatter(uid):
+    if _is_legacy_uid(uid):
+        return _format_legacy_uri
+
+    return _format_uri
+
+
 class GutenbergEbooks(api.TextSource):
     """Implementation of api.TextSource that fetches books from Project
     Gutenberg.
@@ -32,20 +75,8 @@ class GutenbergEbooks(api.TextSource):
                 yield graph
 
     def _format_remote_uris(self, text_info):
-        if 0 < text_info.uid < 10:
-            basic_url = '{root}/{path}/{file}.txt'.format(
-                root=r'http://www.gutenberg.lib.md.us',
-                path='etext90',
-                file=["when11", "bill11", "jfk11", "getty11",
-                      "const11", "liber11", "mayfl11",
-                      "linc211", "linc111"][text_info.uid-1])
-        else:
-            uid = str(text_info.uid)
-            basic_url = '{root}/{path}/{uid}/{uid}.txt'.format(
-                root=r'http://www.gutenberg.lib.md.us',
-                path='/'.join(uid[:len(uid)-1]),
-                uid=text_info.uid)
-        yield basic_url
+        uri_formatter = remote_uri_formatter(text_info.uid)
+        yield uri_formatter(text_info.uid)
 
     def textinfo_converter(self, rdf_graph):
         ebook = next(iter(rdf_graph.query('''
