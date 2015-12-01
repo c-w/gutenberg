018913e4b518648b832d057169746e3976f39761
Clemens Wolff <clemens.wolff+git@gmail.com>
2015-12-01 03:12:27 +0000

    Bump version number post release
    Resolves #30

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup
 
 setup(
     name='Gutenberg',
-    version='0.4.0',
+    version='0.4.1',
     author='Clemens Wolff',
     author_email='clemens.wolff+pypi@gmail.com',
     packages=find_packages(exclude=['tests']),
