527c07fc93f30135b14af0295a7898e695201df6
Casey Peel <cpeel@users.noreply.github.com>
2016-01-07 06:55:36 -0800

    Specify the container type in the extractor
    Unlike title and author, the desired data for formaturi is in the
    URIRef, not the Literal. Allow Extractors to specify where their data
    is located.

--- gutenberg/query/extractors.py
+++ gutenberg/query/extractors.py
@@ -4,6 +4,7 @@
 from __future__ import absolute_import
 
 from rdflib.term import Literal
+from rdflib.term import URIRef
 
 from gutenberg._domain_model.vocabulary import DCTERMS
 from gutenberg._domain_model.vocabulary import PGTERMS
@@ -32,7 +33,7 @@ class _SimplePredicateRelationshipExtractor(MetadataExtractor):
 
     @classmethod
     def get_etexts(cls, requested_value):
-        query = cls._metadata()[:cls.predicate():Literal(requested_value)]
+        query = cls._metadata()[:cls.predicate():cls.contains(requested_value)]
         return frozenset(cls._uri_to_etext(result) for result in query)
 
 
@@ -48,6 +49,10 @@ class AuthorExtractor(_SimplePredicateRelationshipExtractor):
     def predicate(cls):
         return DCTERMS.creator / PGTERMS.alias
 
+    @classmethod
+    def contains(cls, value):
+        return Literal(value)
+
 
 class TitleExtractor(_SimplePredicateRelationshipExtractor):
     """Extracts book titles.
@@ -61,6 +66,10 @@ class TitleExtractor(_SimplePredicateRelationshipExtractor):
     def predicate(cls):
         return DCTERMS.title
 
+    @classmethod
+    def contains(cls, value):
+        return Literal(value)
+
 
 class FormatURIExtractor(_SimplePredicateRelationshipExtractor):
     """Extracts book format URIs.
@@ -73,3 +82,7 @@ class FormatURIExtractor(_SimplePredicateRelationshipExtractor):
     @classmethod
     def predicate(cls):
         return DCTERMS.hasFormat
+
+    @classmethod
+    def contains(cls, value):
+        return URIRef(value)
