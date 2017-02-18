c8d410101be6f5d041bcb46738df914e80c70f44
Clemens Wolff <clemens.wolff+git@gmail.com>
2017-02-18 15:28:54 -0800

    Move type binding to extractors
    This localizes all the code for every extractor into just one place
    which makes it easier to grasp all the things that the extractor does.
    For example, with this structure, it's obvious that the language
    extractor needs to bind the RFC 4646 type to string.

--- gutenberg/_domain_model/types.py
+++ gutenberg/_domain_model/types.py
@@ -1,7 +1,12 @@
 """Module to deal with type validation."""
 
 from __future__ import unicode_literals
+import sys
+
+from rdflib.term import bind
+
 from gutenberg._domain_model.exceptions import InvalidEtextIdException
+
 
 def validate_etextno(etextno):
     """
@@ -13,3 +18,13 @@ def validate_etextno(etextno):
     if not isinstance(etextno, int) or etextno <= 0:
         raise InvalidEtextIdException
     return etextno
+
+
+def rdf_bind_to_string(rdf_type):
+    """
+    Python2/3 compatibility wrapper around rdflib.term.bind that binds a term to
+    the approrpiate string type.
+
+    """
+    string_type = unicode if sys.version_info < (3,) else str
+    bind(rdf_type, string_type)
