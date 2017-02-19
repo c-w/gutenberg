bae4e5b07317aff3a4a5ff544ebb3adc2697f0ba
Clemens Wolff <clemens.wolff+git@gmail.com>
2017-02-18 23:53:14 -0800

    Fix rdflib-sqlalchemy dependency link

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ dependency_links = links_general | links_version
 
 setup(
     name='Gutenberg',
-    version='0.4.4',
+    version='0.4.5',
     author='Clemens Wolff',
     author_email='clemens.wolff+pypi@gmail.com',
     packages=find_packages(exclude=['tests']),
