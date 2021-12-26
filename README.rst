29f8b9383ee84dfb8cb52bfe78073a808cabae88
Hugo van Kemenade <hugovk@users.noreply.github.com>
2021-12-26 23:43:16 +0200

    Drop support for EOL Python 3.6

--- README.rst
+++ README.rst
@@ -30,7 +30,7 @@ The functionality provided by this package includes:
 * Cleaning the texts: removing all the crud, leaving just the text behind.
 * Making meta-data about the texts easily accessible.
 
-The package has been tested with Python 3.6+.
+The package has been tested with Python 3.7+.
 
 An HTTP interface to this package exists too.
 `Try it out! <https://github.com/c-w/gutenberg-http>`_
--- setup.py
+++ setup.py
@@ -32,12 +32,11 @@ setup(
     description='Library to interface with Project Gutenberg',
     long_description=long_description,
     install_requires=sorted(install_requires),
-    python_requires='>=3.6',
+    python_requires='>=3.7',
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
